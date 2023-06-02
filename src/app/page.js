2a14cbc7263f05aabe2dcc22f0bbadda23d6741c
Mario Carballo Zama <afk@ellugar.co>
2023-06-01 20:35:37 -0600

    initial setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,57 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Octokit } from "octokit";
 
-export default function Home() {
+const API_URL = "https://api.github.com";
+const REPO_OWNER = "godotengine";
+const REPO = "godot";
+const REPO_PATH = "editor/icons";
+const API_TOKEN = process.env.API_TOKEN;
+const iconRawPrefix = `https://raw.githubusercontent.com/${REPO_OWNER}/${REPO}/master/${REPO_PATH}`;
+
+const url = `${API_URL}/repos/${REPO_OWNER}/${REPO}/contents/${REPO_PATH}`;
+
+async function getData() {
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  });
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <header>
+        <h1>Godot Editor Icons</h1>
+      </header>
+      <section>
+        {data
+          .filter((item) => item.name.split(".")[1] === "svg")
+          .map((item) => {
+            return (
+              <figure key={item.name}>
+                <div>
+                  <img
+                    src={`https://raw.githubusercontent.com/godotengine/godot/master/editor/icons/${item.name}`}
+                    alt={item.name}
+                  />
+                </div>
+                <figcaption>
+                  <span>{item.name}</span>
+                </figcaption>
+              </figure>
+            );
+          })}
+      </section>
     </main>
-  )
+  );
 }
